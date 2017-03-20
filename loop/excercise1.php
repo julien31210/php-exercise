5e952223f9d46d1711f3f02ede55fc2be190ef96
Julien <julien.linder@gmail.com>
2017-03-20 14:15:03 +0100

    premier excercice

---
+++ loop/excercise1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop</title>
+</head>
+<body>
+	<?php 
+		echo "Hello World!";
+		$compteur;
+		echo "$compteur";
+		for ($compteur = 0; $compteur <= 10; $compteur++){
+			echo "$compteur"." " ;
+		}
+	 ?>
+</body>
+</html>
