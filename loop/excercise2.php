9284971c0f8406057dc5978a695905d44bac0128
Julien <julien.linder@gmail.com>
2017-03-20 16:24:44 +0100

    deuxième excercice fait

---
+++ loop/excercise2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop</title>
+</head>
+<body>
+	<?php 
+		$num1;
+		$num2 = 5;
+			for ($num1 = 0; $num1 <= 20; $num1 ++){
+				echo $num1* $num2." ";
+		}
+
+	?>
+</body>
+</html>
