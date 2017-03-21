c9235a8bf0028973f866dfb54154ce13932e8833
Julien <julien.linder@gmail.com>
2017-03-21 14:37:50 +0100

    exo 5 fait

---
+++ fonction/excercise5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>fonction1</title>
+</head>
+<body>
+	<?php 
+		function nombre($nb2, $string = " autres"){
+			return $nb2.$string;
+		};
+		print nombre(2);
+	 ?>
+</body>
+</html>
