611441fcea0132337c1301c32406ad04caaca273
Julien <julien.linder@gmail.com>
2017-03-21 12:14:42 +0100

    excecise8fait

---
+++ loop/excercise8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>loop3</title>
+</head>
+<body>
+	<?php
+		echo "Hello World!"."<br>";
+		$num1;
+		for ($num1=200; $num1 >= 0; $num1-=12) { 
+			echo $num1."<br>"." Enfin!!! <br> ";
+		}
+	 ?>
+</body>
+</html>
