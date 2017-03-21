7787f56288d7324334bc28654b020c93e0e869da
Julien <julien.linder@gmail.com>
2017-03-21 16:35:06 +0100

    exo 5 fait

---
+++ array/excercise5.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>array</title>
+</head>
+<body>
+	<?php 
+		$hautDeFrance= array("59" =>"Nord", "62" =>"Pas de calais", "80" =>"Somme", "60" =>"Oise", "02" =>"Aisne");
+	 ?>
+</body>
+</html>
