ad3964ac235d9b2886dce4e7670b60744cc0510f
Julien <julien.linder@gmail.com>
2017-03-21 16:35:54 +0100

    exo 6 fait

---
+++ array/excercise6.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>array</title>
+</head>
+<body>
+	<?php 
+		$hautDeFrance= array("59" =>"Nord", "62" =>"Pas de calais", "80" =>"Somme", "60" =>"Oise", "02" =>"Aisne");
+
+		echo $hautDeFrance[59];
+	 ?>
+</body>
+</html>
