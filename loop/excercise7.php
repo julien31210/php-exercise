adea54997eb3abea8297d0d1536fcf918f2a9403
Julien <julien.linder@gmail.com>
2017-03-21 12:04:04 +0100

    excecise7fait

---
+++ loop/excercise7.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>loop3</title>
+</head>
+<body>
+	<?php
+		echo "Hello World!"."<br>";
+		$num1;
+		for ($num1=1; $num1 <= 100 ; $num1+=15) { 
+			echo $num1."<br>"." C'est presque bon.<br> ";
+		}
+	 ?>
+</body>
+</html>
