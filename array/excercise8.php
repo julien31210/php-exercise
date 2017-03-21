48f9833bb92873974890614c949912b9bda2e730
Julien <julien.linder@gmail.com>
2017-03-21 16:50:36 +0100

    exo 8 fait

---
+++ array/excercise8.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>array</title>
+</head>
+<body>
+	<?php 
+		$mois = ["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre"];
+		foreach ($mois as $moi) {
+			echo $moi." ";
+		}
+	 ?>
+</body>
+</html>
