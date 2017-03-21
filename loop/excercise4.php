5dd4467b8458bb0bbc58f2f6049c190a31992c66
Julien <julien.linder@gmail.com>
2017-03-21 11:46:06 +0100

    excecise4fait

---
+++ loop/excercise4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>loop3</title>
+</head>
+<body>
+	<?php
+		echo "Hello World!"."<br>";
+		$num1;
+		for ($num1=1; $num1 <=10 ; $num1++) { 
+			echo $num1."<br>";
+			$num1 = $num1*1.5;
+		}
+	 ?>
+</body>
+</html>
