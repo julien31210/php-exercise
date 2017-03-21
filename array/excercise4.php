3c7f960020e8dca3f91816c5e5d440f73c346a33
Julien <julien.linder@gmail.com>
2017-03-21 16:07:55 +0100

    exo 4 fait

---
+++ array/excercise4.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>array</title>
+</head>
+<body>
+	<?php 
+		$mois = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+		print $mois[7]="août";
+	 ?>
+</body>
+</html>
