a4958b3e3c9547bdb3e92642f882eecab3cac561
Julien <julien.linder@gmail.com>
2017-03-22 14:16:31 +0100

    exo 1 fait

---
+++ Form/excercise1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Formulaire</title>
+</head>
+<body>
+<form action="user.php" method="GET">
+ <p>Votre nom : <input type="text" name="nom" /></p>
+ <p>Votre âge : <input type="text" name="age" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+	<?php 
+		
+	 ?>
+</body>
+</html>
